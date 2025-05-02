6fb2ff97102b32d6731f0bc3348d4f0181488753
Daniel Medina Cossio <paginamedina@gmail.com>
2025-05-02 17:18:14 -0500

    [USER] fixing dto for users

--- users/dtos/CreateUserDto.js
+++ users/dtos/CreateUserDto.js
@@ -9,6 +9,41 @@ class CreateUserDto {
     this.sub = data.sub;
     this.password = data.password;
   }
+
+  validate() {
+    const errors = [];
+
+    // Validar email
+    if (!this.email) {
+      errors.push('El email es requerido');
+    } else if (!this.isValidEmail(this.email)) {
+      errors.push('El formato del email es inválido');
+    }
+
+    // Validar nombre
+    if (!this.name) {
+      errors.push('El nombre es requerido');
+    }
+
+    // Si se está creando un usuario nuevo (no actualización), validar password
+    if (this.isNewUser() && !this.password) {
+      errors.push('La contraseña es requerida para nuevos usuarios');
+    }
+
+    // Puedes agregar más validaciones según tus requerimientos
+
+    return errors;
+  }
+
+  isValidEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  }
+
+  isNewUser() {
+    // Si no tiene sub o id, asumimos que es un usuario nuevo
+    return !this.sub;
+  }
 }
 
 module.exports = CreateUserDto;
